0fc18b29e626124dec28bded54d14cc1e0f4c123
Andy C <acobley@computing.dundee.ac.uk>
2014-01-25 17:23:40 +0000

    Adding Fault servlet that will use a JNDI data source

---
+++ ac32007examples/src/uk/ac/dundee/computing/aec/servlets/Fault.java
@@ -0,0 +1,50 @@
+package uk.ac.dundee.computing.aec.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class Fault
+ */
+@WebServlet(
+		urlPatterns = { 
+				"/Fault", 
+				"/Fault/*"
+		}, 
+		initParams = { 
+				@WebInitParam(name = "data-source", value = "jdbc/Fault.db")
+		})
+public class Fault extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private DataSource _ds = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Fault() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
