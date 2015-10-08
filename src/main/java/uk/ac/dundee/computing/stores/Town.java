4292c0a5dc2ba96bbde1a0ba2b8295855484aae8
andy <andy@192.168.0.100>
2015-10-08 15:40:57 +0100

    added county to towns

--- src/main/java/uk/ac/dundee/computing/stores/Town.java
+++ src/main/java/uk/ac/dundee/computing/stores/Town.java
@@ -5,6 +5,7 @@ public class Town {
    private double lat;
    private double lng;
    private int population;
+   private String county;
 	public Town(){
 		
 	}
@@ -14,6 +15,15 @@ public class Town {
 		this.lat=lat;
 		this.lng=lng;
 		this.population=Population;
+		return false;
+	}
+        
+        public boolean setTown(String Name, double lat, double lng,int Population,String County){
+		name=Name;
+		this.lat=lat;
+		this.lng=lng;
+		this.population=Population;
+                this.county=county;
 		return false;
 	}
 	
@@ -32,5 +42,10 @@ public class Town {
 	public int getPopulation(){
 		return population;
 	}
+        public String getcounty(){
+            return county;
+        }
+        
+        
 }
 
