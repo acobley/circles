f196750797e8dfcfe12cdecd4f3b8e82594a4983
andy <andy@172.19.73.214>
2017-09-26 12:27:45 +0100

    updated circle generator

--- src/main/webapp/scripts/ex11.js
+++ src/main/webapp/scripts/ex11.js
@@ -25,7 +25,7 @@ circles.attr("cx", function(d){
 }
 
 function loadData(){
-   d3.json("http://ac32007.cloudapp.net:8080/Circles/Circles/10",function(error,data){
+   d3.json("http://35.188.184.73/Circles/10",function(error,data){
    if (error){
       console.log(error)
    }else{
